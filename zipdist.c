e5100b6ee1e3359143c7142089e690dd3503b2ed
Ed Harcourt <edharcourt@stlawu.edu>
2021-01-29 14:14:55 -0500

    .

--- zipdist.c
+++ zipdist.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
+double to_radians(double degrees) {
+    return degrees*M_PI/180.0;
+}
+
+// https://nathanrooy.github.io/posts/2016-09-07/haversine-with-python/
+double distance(double lat1, double lon1,
+                double lat2, double lon2) {
+
+    double phi1 = to_radians(lat1);
+    double phi2 = to_radians(lat2);
+    double delta_phi = to_radians(lat2 - lat1);
+    double delta_gamma = to_radians(lon2 - lon1);
+ 
+    double x = pow(sin(delta_phi / 2), 2) + 
+               cos(phi1) * cos(phi2) *
+               pow(sin(delta_gamma / 2), 2);
+
+    double y = 2 * atan2(sqrt(x), sqrt(1-x)); 
+
+    double R = 3958.8; // miles
+    
+    return R * y;
+
+}
+
+// TODO break up main
 int main(int argc, char *argv[]) {
     // Debug
     // printf("%d %s %s %s\n", argc, argv[0], argv[1], argv[2]);
@@ -51,9 +78,14 @@ int main(int argc, char *argv[]) {
             got++;
         }	
     } // while
+
     printf("%s %s, %s, (Latitude) %lf, (Longitude) %lf\n", 
             argv[1], city1, state1, lat1, lon1);  
     printf("%s %s, %s, (Latitude) %lf, (Longitude) %lf\n", 
             argv[2], city2, state2, lat2, lon2);  
+
+    double d = distance(lat1, lon1, lat2, lon2);
+    printf("The distance between %s, %s and %s, %s, is %.1lf miles.\n", 
+            city1, state1, city2, state2, d);
 
 } // main
